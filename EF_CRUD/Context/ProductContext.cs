bb6e2b83d1b918d8f2d7fa677bc8e0e9990283e4
Administrator <Administrator@ANGEL-PC>
2020-09-17 16:29:23 +0530

    Add project files.

---
+++ EF_CRUD/Context/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using EF_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EF_CRUD.Context
+{
+    public class ProductContext: DbContext 
+    {
+        public DbSet<Product> products { get; set; }
+
+      
+    }
+}
